5de5ae0c2b5507c3c7a4d34f79a1a877ff5ee4d2
h0bb1t <idegkov@gmail.com>
2024-06-07 20:15:44 +0300

    sc

--- src/utils.sh
+++ src/utils.sh
@@ -23,11 +23,11 @@ function generate_left_side_string() {
 
 function generate_inactive_window_string() {
 	# local separator_start="#[bg=${PALLETE['dark5']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
-	local separator_start="#[bg=${PALLETE['blue7']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
-	local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['blue7']}]${left_separator:?}#[none]"
+	local separator_start="#[bg=${PALLETE['green2']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
+	local separator_internal="#[bg=${PALLETE['dark3']},fg=${PALLETE['green2']}]${left_separator:?}#[none]"
 	local separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['dark3']}]${left_separator:?}#[none]"
 
-	echo "${separator_start}#[fg=${PALLETE[green2]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag, , }#W ${separator_end}"
+	echo "${separator_start}#[fg=${PALLETE['blue7']}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag, , }#W ${separator_end}"
 }
 
 function generate_active_window_string() {
@@ -35,8 +35,8 @@ function generate_active_window_string() {
 	# separator_internal="#[bg=${PALLETE['purple']},fg=${PALLETE['magenta']}]${left_separator:?}#[none]"
 	# separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['purple']}]${left_separator:?}#[none]"
 	separator_start="#[bg=${PALLETE['orange']},fg=${PALLETE['bg_highlight']}]${left_separator:?}#[none]"
-	separator_internal="#[bg=${PALLETE['blue']},fg=${PALLETE['orange']}]${left_separator:?}#[none]"
-	separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['blue']}]${left_separator:?}#[none]"
+	separator_internal="#[bg=${PALLETE['blue6']},fg=${PALLETE['orange']}]${left_separator:?}#[none]"
+	separator_end="#[bg=${PALLETE[bg_highlight]},fg=${PALLETE['blue6']}]${left_separator:?}#[none]"
 
-	echo "${separator_start}#[fg=${PALLETE[terminal_black]}]#I${separator_internal}#[fg=${PALLETE[white]}] #{?window_zoomed_flag, , }#W ${separator_end}#[none]"
+	echo "${separator_start}#[fg=${PALLETE[terminal_black]}]#I${separator_internal}#[fg=${PALLETE[terminal_black]}] #{?window_zoomed_flag, , }#W ${separator_end}#[none]"
 }
